aba7c532ca16e361dc8bc2495ce8ca69cf6d5b24
gudtogo <earias_1995@hotmail.com>
2021-10-23 21:55:23 -0300

    Tabla de revisor

---
+++ src/components/TablaRevisor.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="TablaRevisor">
+        <div style="padding:50px; margin-left:300px">
+           <table class="table table-sm table-hover zui-table-rounded" >
+          <thead style="color: white;background-color: #514BD5;">
+            <tr>
+              <th scope="col">N° Documento</th>
+              <th scope="col">Oficina</th>
+              <th scope="col">N° Oficina</th>
+              <th scope="col">Fecha</th>
+              <th scope="col">Estado</th>
+              <th scope="col">Asignada</th>
+              <th scope="col">Apellido</th>
+              <th scope="col">Rol</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <th scope="row">Oficina L</th>
+              <th scope="row">10</th>
+              <th scope="row">19/08/2021</th>
+              <td> 
+                  <div class="btn-group" role = "group" aria-label="Basic example">
+                      <button type="button" class="rounded-pill" style="background-color:grey">Esperando Pago</button>
+                  </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+       
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TablaRevisor',
+  props: {
+        opcion:Array,
+        username:{
+            type: String,
+            default: 'Hola!'
+        },
+        rol :  {
+            type: String,
+            default: "REVISOR"
+        }
+  },
+}
+
+</script>
+
+<style scoped>
+.zui-table-rounded thead th:first-child {
+    border-radius: 10px 0 0 0;
+}
+.zui-table-rounded thead th:last-child {
+    border-radius: 0 10px 0 0;
+}
+
+.zui-table-rounded tbody tr:last-child td:first-child {
+    border-radius: 0 0 0 10px;
+}
+.zui-table-rounded tbody tr:last-child td:last-child {
+    border-radius: 0 0 10px 0;
+}
+</style>
