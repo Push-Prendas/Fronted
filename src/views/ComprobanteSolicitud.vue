a046f9af138a1b753d4bbf682f4bcb0c1810fdcc
gudtogo <earias_1995@hotmail.com>
2021-10-24 01:34:24 -0300

    View para comprobante de solicitud

---
+++ src/views/ComprobanteSolicitud.vue
@@ -0,0 +1,72 @@
+<template>
+    <div id="dashboard">
+        <Menu :opciones= opcion />
+        <Navbar :username= username />
+        <div class="right">
+            <AntecedentesComprobante />
+            <AcreedorComprobante />
+            <ConstituyentesComprobante />
+            <DeudoresComprobante />
+            <RequirientesComprobante />
+            <div class="row d-flex justify-content-center" id="contenedor">
+                <button class="col-2 titleButton">Imprimir</button>
+            </div>
+        </div>
+        
+        
+    </div>
+</template>
+<script scoped>
+import AntecedentesComprobante from '../components/AntecedentesComprobante.vue'
+import AcreedorComprobante from '../components/AcreedorComprobante.vue'
+import ConstituyentesComprobante from '../components/ConstituyentesComprobante.vue'
+import DeudoresComprobante from '../components/DeudoresComprobante.vue'
+import RequirientesComprobante from '../components/RequirentesComprobante.vue'
+
+
+
+export default {
+  name: 'formularioModificacion',
+  data() {
+        return {
+            opcion: opciones,
+            username: usernameGlobal,
+        }
+    },
+  components: {
+    AntecedentesComprobante,
+    AcreedorComprobante,
+    ConstituyentesComprobante,
+    DeudoresComprobante,
+    RequirientesComprobante
+  },
+  
+}
+</script>
+
+<style scoped>
+#contenedor{
+    width: 60em;
+    margin-left: 34%;
+    margin-top: 2%;
+}
+.right {
+    margin-right: 10px;
+    float: right;
+    width: 100%;
+    height: auto;
+}
+
+
+.titleButton{
+    color: #514BD5;
+    font-family: Roboto;
+    font-weight: bold;
+    background: #CEF1FF;
+    border-radius: 15em;
+    width: 10em;
+    margin-bottom: 2em;
+    margin-left: 2em;
+    margin-right: 2em;
+}
+</style>
